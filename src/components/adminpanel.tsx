e712234f7b366d8e9d02cb99d9aea74994c677ff
evgeniy <konopkin11@gmail.com>
2022-05-31 22:25:12 +0300

    admin control added

--- src/components/adminpanel.tsx
+++ src/components/adminpanel.tsx
@@ -3,10 +3,56 @@ import {useEffect, useState} from "react";
 import {getUserByLogin} from "./firebase/api";
 
 let settingsSlidesText = []
+let currentSlide = 0;
 const Adminpanel = () => {
 
   const [slidesCount, setSlidedCount] = useState(2);
 
+  function changeSlide(e){
+      console.log(e.target.id)
+      let totalSlides = parseInt(document.querySelector<HTMLInputElement>("#slidesCount").value) - 1 ;
+      let text = document.querySelector<HTMLInputElement>("#slideText").value;
+      let id = e.target.id;
+
+      if (id === "getNextSlide"){
+          if(currentSlide == totalSlides){
+              settingsSlidesText[currentSlide] = text;
+          }else{
+              settingsSlidesText[currentSlide] = text;
+              currentSlide++;
+              if(settingsSlidesText[currentSlide] == undefined){
+                  document.querySelector<HTMLInputElement>("#slideText").value = ""
+              }else
+              document.querySelector<HTMLInputElement>("#slideText").value = settingsSlidesText[currentSlide];
+          }
+      }else if(id ==="getPreviousSlide"){
+          if(currentSlide == 0){
+              settingsSlidesText[currentSlide] = text;
+          }else{
+              settingsSlidesText[currentSlide] = text;
+              currentSlide--;
+              if(settingsSlidesText[currentSlide] == undefined){
+                  document.querySelector<HTMLInputElement>("#slideText").value = ""
+              }else
+                  document.querySelector<HTMLInputElement>("#slideText").value = settingsSlidesText[currentSlide]
+          }
+      }
+      document.querySelector<HTMLInputElement>("#currentSlide").innerHTML = currentSlide.toString();
+
+      console.log(settingsSlidesText)
+  }
+
+  function confirmSettingsText() {
+      let totalSlides = parseInt(document.querySelector<HTMLInputElement>("#slidesCount").value) - 1
+      let text = document.querySelector<HTMLInputElement>("#slideText").value;
+     settingsSlidesText[currentSlide] = text;
+     for(let i = currentSlide; i<totalSlides;i++){
+         let text = document.querySelector<HTMLInputElement>("#slideText").value;
+         if(settingsSlidesText[i] == undefined) settingsSlidesText[i] = "";
+     }
+     console.log(settingsSlidesText)
+
+  }
 
 
   useEffect(()=>{
@@ -16,14 +62,23 @@ const Adminpanel = () => {
     fetchData();
   }, [])
 
-  return (
+
+
+
+    return (
       <div style={{"color":"white"}}>
-      <div className="rightMenu" >
+
         <div className="rulesSettings">
-          <p>Slides count</p>
-          <input type="number" id="slidesCount" />
+            <h1>Rules settings</h1>
+            <p>Slides count</p>
+            <input type="number" id="slidesCount" />
+            <input type="submit" id="getPreviousSlide" value="Previous slide" onClick={changeSlide}/>
+            <input type="text" id="slideText"  placeholder="type text for slide here"/>
+            <input type="submit" id="getNextSlide" value="Next slide" onClick={changeSlide}/>
+            <input type="submit"  value="Confirm all" onClick={confirmSettingsText}/>
+            <p id="currentSlide">0</p>
         </div>
-      </div>
+
       </div>)
 
 }
